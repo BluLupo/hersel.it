94045988d97c2701f1738a9c036184d07f39822e
@BluLupo <hersel1991@gmail.com>
2024-01-10 15:15:29 +0100

    add logic blog

--- routes/blog.py
+++ routes/blog.py
@@ -3,22 +3,27 @@
 
 # Copyright Hersel Giannella
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask_paginate import Pagination, get_page_args
 from database.methods.articles import get_articles_and_authors_raw, get_article_by_id
-#from flask_paginate import Pagination, get_page_args
+from utilities.aboutclass import BluLupo
 
 route_blog = Blueprint('route_blog', __name__)
 
 @route_blog.route('/blog', methods=['GET', 'POST'])
 def blog():
     # Chiamata alla funzione
+    me = BluLupo()
     articles_and_authors = get_articles_and_authors_raw()
-    get_article = get_article_by_id(1)
-    print(get_article)
-    print(articles_and_authors)
 
-    # Cicla e stampa ogni articolo
-    for article in articles_and_authors:
-        print(article['title'])
-    #print(b)
-    return render_template('blog.html',articles=articles_and_authors)
+    # Paginazione
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    per_page = 3  # Numero di articoli per pagina
+    total = len(articles_and_authors)
+    # Calcola l'offset per recuperare gli articoli corretti
+    offset = (page - 1) * per_page
+    pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
+    # Estrai gli articoli per la pagina corrente
+    articles_for_page = articles_and_authors[offset: offset + per_page]
+
+    return render_template('blog.html', articles=articles_for_page, pagination=pagination,me=me)
--- routes/home.py
+++ routes/home.py
@@ -5,24 +5,10 @@
 
 from flask import Blueprint, render_template
 from utilities.aboutclass import BluLupo
-from database.methods.user import create_user,get_users, get_user_by_id, get_user_by_username
 
 route_home = Blueprint('route_home', __name__)
 
 @route_home.route('/', methods=['GET', 'POST'])
 def home():
     me = BluLupo()
-    result = get_users()
-
-    # Stampa i dettagli di tutti gli utenti
-    for user_details in result:
-        print(user_details['username'])
-
-    user_id_to_get = 1  # Sostituisci con l'ID desiderato
-    single_result = get_user_by_id(user_id_to_get)
-    single_result_username = get_user_by_username('hersel')
-    print(single_result)
-    print(single_result_username)
-
-
-    return render_template('home.html',user=result,me=me)
+    return render_template('home.html',me=me)
