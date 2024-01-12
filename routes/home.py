64d89ba5540f179ee4bd53632c52184047236c94
@BluLupo <hersel1991@gmail.com>
2024-01-12 12:50:13 +0100

    fix HSTS

--- routes/home.py
+++ routes/home.py
@@ -3,12 +3,19 @@
 
 # Copyright Hersel Giannella
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, make_response
 from utilities.aboutclass import BluLupo
 
 route_home = Blueprint('route_home', __name__)
 
 @route_home.route('/', methods=['GET', 'POST'])
+@route_home.route('/home', methods=['GET', 'POST'])
+@route_home.route('/index.php', methods=['GET', 'POST'])
 def home():
     me = BluLupo()
-    return render_template('home.html',me=me)
+    response = make_response(render_template('home.html',me=me))
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+
+    return response
