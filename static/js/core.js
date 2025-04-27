9fa02fe15b17e089e6459e918680b1141b570b44
Blulupo <hersel1991@gmail.com>
2025-04-27 21:42:14 +0200

    Fix Javascript

--- static/js/core.js
+++ static/js/core.js
@@ -1,4 +1,4 @@
-        // Navbar color change on scroll
+// Navbar color change on scroll
         window.addEventListener('scroll', function() {
             const navbar = document.getElementById('navbar');
             if (window.scrollY > 50) {
